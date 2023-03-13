72cd743a5fb9002b2fda2943c6fa17facd416532
dharumaraj <dharumaraj30092000@gmail.com>
2023-03-13 17:01:55 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var name="amma";
+var str="";
+for(let i=name.length-1;i>=0;i--){
+	str+=name[i];
+}
+if(str===name){
+	console.log("this is palindrome");
+}
+else {
+	console.log("this is not palindrome");
+}
